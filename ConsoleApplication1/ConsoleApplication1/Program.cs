26ab0d5f3a5f3e1a631cb955d3aa275246bd8db2
alonso <alonsobugs@gmail.com>
2016-09-17 14:53:04 -0600

    Componentizacion del Metodo para generar el mensaje de Hello World.

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -8,14 +8,25 @@ namespace ConsoleApplication1
 {
     class Program
     {
-        
+
+
+        static String GenerateHelloWorld(String p_sUserName)
+        {
+            String sResult;
+            sResult = "Hello World " + p_sUserName;
+
+            return sResult;
+        }
+
+
+
         static void Main(string[] args)
         {
 
             String sUserName;
             Console.WriteLine("Escriba el nombre de usuario: ");
             sUserName = Console.ReadLine();
-            Console.WriteLine("Hello World,  " + sUserName);
+            Console.WriteLine(GenerateHelloWorld(sUserName));
             Console.ReadLine();
 
         }
