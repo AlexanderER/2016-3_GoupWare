9a9725155224d077d14cf28ad2b0e06decc52736
Alexander <alexer1515@gmail.com>
2016-10-22 16:22:29 -0600

    Entity Framework, y creacion de dos metodos anidados

---
+++ GoupWare.AdventureWorks.Demo/GoupWare.AdventureWorks.Model/MyTables/Product.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoupWare.AdventureWorks.Model
+{
+    public partial class Product
+    {
+        public Double Antiguedad { get
+                                    {
+                                        Double Antiguedad = 0.0;
+                                        DateTime? miFechaFinal = DateTime.Now;
+
+                                        if (DiscontinuedDate != null)
+                                        {
+                                            miFechaFinal = DiscontinuedDate;
+                                        }
+
+                                        TimeSpan ts = (TimeSpan)(miFechaFinal - SellStartDate);
+                                        Antiguedad = ts.Days;
+
+                                        return Antiguedad;
+                                    }
+                                }
+
+        //--------------------------------------------------------------------------------------------------
+
+        public Double WeightInKilos {
+                                        get {
+                                                Double dPesoEnKilos = 0.0;
+
+                                                if (Weight != null)
+                                                    {
+                                                        switch (WeightUnitMeasureCode)
+                                                        {
+                                                            case "G":
+                                                                dPesoEnKilos = (Convert.ToDouble(Weight) / 1000);
+                                                                break;
+
+                                                            case "LB":
+                                                                dPesoEnKilos = (Convert.ToDouble(Weight) / 2.20462);
+                                                                break;
+
+                                                            case "NULL":
+                                                                dPesoEnKilos = Convert.ToDouble(Weight);
+                                                                break;
+                                                        }
+                                                    }                                
+
+                                                return dPesoEnKilos;
+                                            }
+                                    }
+
+
+
+
+    }
+}
